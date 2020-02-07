c933134fcc9f2650d7cb22afc6c9f0d9dba99f00
Mike Freeman <michael.k.freeman@gmail.com>
2020-02-06 16:41:29 -0800

    Draw lines as svg

--- js/sketch.js
+++ js/sketch.js
@@ -2,11 +2,15 @@
 let img;
 let fitToScreen = true;
 let lineLengthSlider, xSpacingSlider, ySpacingSlider, strokeWeightSlider, lineDirectionSelect, modeSelect, colorSelect, windowWidth, windowHeight;
-
+const namespace = 'http://www.w3.org/2000/svg';
+let svg;
 // Load obama on start
 function preload() {
     // preload() runs once
     img = loadImage('imgs/obama.jpg');
+    svg = document.querySelector("#svg-clone")
+    svg.style.position = "absolute";
+    svg.style.left = "300px";
 }
 
 function imageUpload(file) {
@@ -58,7 +62,7 @@ function makeControls() {
     imgPreview.parent(controlWrapper);
 
     // Mode (line, circles, rectangles)
-    modeSelect = makeSelect("Drawing Mode", options = ["Lines", "Circles", "Rectangles"], value = "Circles", parent = controlWrapper, drawOnce)
+    modeSelect = makeSelect("Drawing Mode", options = ["Lines", "Circles", "Rectangles"], value = "Lines", parent = controlWrapper, drawOnce)
     // Spacing
     let spacingHeader = createDiv("<h3>Spacing</h3>");
     spacingHeader.parent(controlWrapper);
@@ -99,8 +103,10 @@ function download() {
 // Set up (elements only drawn once)
 function setup() {
     // Get window size 
-    windowWidth = window.innerWidth - 270;
-    windowHeight = window.innerHeight - 180;
+    // windowWidth = window.innerWidth - 270;
+    // windowHeight = window.innerHeight - 180;
+    windowWidth =  96 * 8;
+    windowHeight = 96 * 6;
 
     // Container for everything
     let container = createDiv().class("container");
@@ -112,7 +118,7 @@ function setup() {
     canvas = createCanvas(windowWidth, windowHeight).class("p5_canvas");
     canvasContainer.parent(container);
     canvas.parent(canvasContainer);
-
+    canvas.style("display", "none")
     // Set color mode to RGB percentages  
     colorMode(RGB, 100);
     drawOnce();
@@ -129,10 +135,20 @@ function getGrayscaleValue(img, x, y) {
         )
     };
 }
-
+function addSvgLine(x1, y1, x2, y2) {        
+    svg.setAttribute("width", windowWidth);
+    svg.setAttribute("height", windowHeight);
+    let line = document.createElementNS(namespace, 'line');
+    line.style.stroke = "rgb(0,0,0)";
+    line.setAttribute('x1', x1);
+    line.setAttribute('y1', y1);
+    line.setAttribute('x2', x2);
+    line.setAttribute('y2', y2);
+    svg.appendChild(line);
+}
 function drawOnce() {
     background("white");
-
+    svg.innerHTML = '' // clear svg;
     // Resize based on image width
     let dims = getDimensions(img, windowWidth, windowHeight);
     resizeCanvas(dims.width, dims.height)
@@ -149,7 +165,8 @@ function drawOnce() {
             let strokeValue;
             switch (colorSelect.value()) {
                 case "Black and White":
-                    strokeValue = grayValue.value;
+                    // strokeValue = grayValue.value;
+                    strokeValue = "black";
                     break;
                 case "Original":
                     strokeValue = grayValue.original;
@@ -161,10 +178,14 @@ function drawOnce() {
                     strokeWeight(strokeWeightSlider.value());
                     let lineLength = lineLengthSelect.value() == "Constant" ? lineLengthSlider.value() : map(grayValue.value, 0, 100, lineLengthSlider.value(), 1, .1)
                     // let lineLength = 10;
+                    if(lineLength < 5) {
+                        break;
+                    }
                     let direction = lineDirectionSelect.value();
                     let xEnd = direction !== "Vertical" ? x + lineLength : x;
                     let yEnd = direction !== "Horizontal" ? y + lineLength : y;
-                    line(x, y, xEnd, yEnd);
+                    // line(x, y, xEnd, yEnd);
+                    addSvgLine(x, y, xEnd, yEnd);
                     break;
                 case "Circles":
                     stroke(strokeValue);
